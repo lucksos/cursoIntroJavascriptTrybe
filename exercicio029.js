6f1db5a80fb396e053dd89e59737b79d2214fb76
lucksos <luca.trattoria@gmail.com>
2022-02-21 14:41:16 -0300

    Create exercicio029.js

---
+++ exercicio029.js
@@ -0,0 +1,20 @@
+function converteStringParaArray(frase) {
+// não altere a linha acima
+
+ /*
+ * Complete o código abaixo atribuindo na variável resultado
+ * o valor de acordo com as regras do exercício
+ */
+
+    var resultado = []
+    for (var c = 0; c < frase.length; c++) {
+        resultado.push(frase[c])
+    }
+// escreva aqui seu código:
+
+    
+    
+    
+// não altere as linhas abaixo
+  return resultado;
+}
