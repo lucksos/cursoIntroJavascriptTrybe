d66e9ecd642f7d08804123fb5137bc6428c0c676
lucksos <luca.trattoria@gmail.com>
2022-02-21 14:34:08 -0300

    Create exercicio009.js

---
+++ exercicio009.js
@@ -0,0 +1,12 @@
+function soma(elementoA, elementoB) {
+//não altere a linha acima
+    
+/*
+ * Atribua a soma da variável `elementoA` com a variável `elementoB`
+ * na váriavel `resultadoSoma` (já criada abaixo)
+ */
+  var resultadoSoma = elementoA + elementoB;
+
+//não altere as linhas abaixo
+  return resultadoSoma;
+}
