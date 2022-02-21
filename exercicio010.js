9ef3c9a1c2952942dae07bf89f1d8b33ab7ecb04
lucksos <luca.trattoria@gmail.com>
2022-02-21 14:34:42 -0300

    Create exercicio010.js

---
+++ exercicio010.js
@@ -0,0 +1,14 @@
+function subtracao(elementoA, elementoB) {
+//não altere a linha acima
+    
+/*
+ * Atribua a subtração da variável `elementoB` pela variável `elementoA`
+ * na váriavel `resultadoSubtracao` (já criada abaixo)
+ */
+  
+  var resultadoSubtracao = elementoB - elementoA;
+
+//não altere as linhas abaixo
+  return resultadoSubtracao;
+}
+// fim do seu código
