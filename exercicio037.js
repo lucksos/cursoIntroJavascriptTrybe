71d308550c64089b489a74fd7a48aabea8edf024
lucksos <luca.trattoria@gmail.com>
2022-02-21 14:44:54 -0300

    Create exercicio037.js

---
+++ exercicio037.js
@@ -0,0 +1,26 @@
+function buscaNumeroPrimos(n) {
+//não altere a linha acima
+
+/*
+ * Nas linhas a seguir, escreva um código que encontre todos
+ * os números primos entre 0 e n e coloque-os num array chamado
+ * `numerosPrimos`(você deve criar esse array)
+ */ 
+    numerosPrimos = []
+    for (var c = 2; c <= n; c++) {
+        contagemDivisores = 0;
+        for (var d = 1; d <= c; d++) {
+            if (c % d === 0) {
+                contagemDivisores++;
+            }
+        }
+        if (contagemDivisores === 2) { /* Caso o número tenha apenas 2 divisores... */
+            numerosPrimos.push(c); /* ...ele é adicionado à lista */
+        }
+    }
+    
+    
+//não altere as linhas abaixo
+  return numerosPrimos;
+}
+// fim do seu código
