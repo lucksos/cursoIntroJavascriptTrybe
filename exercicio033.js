1e4e9eb8f41a4a290f7802300c3dc7537f8b23e7
lucksos <luca.trattoria@gmail.com>
2022-02-21 14:43:04 -0300

    Create exercicio033.js

---
+++ exercicio033.js
@@ -0,0 +1,14 @@
+/*
+ * Nas linhas abaixo, escreva uma função chamada `calculaIMC``
+ * que receba dois parametros: `altura` e `peso`
+ * e retorne o resultado do calculo de IMC
+ */
+function calculaIMC(altura, peso) {
+    var imc = peso / (altura ** 2);
+    
+    return imc;
+}
+
+
+
+// fim do seu código
