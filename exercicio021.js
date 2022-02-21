cabd0404ceb96254a1bf420074d2cd1d3d779cc9
lucksos <luca.trattoria@gmail.com>
2022-02-21 14:39:09 -0300

    Create exercicio021.js

---
+++ exercicio021.js
@@ -0,0 +1,21 @@
+function verificaParOuImpar(elementoA) {
+// não altere a linha acima
+    
+/* 
+ * Atribua um dos seguintes valores na variável `resultado`
+ *
+ * par: se o valor da variável `elementoA` for par;
+ * impar: se o valor da variável `elementoA` for ímpar;
+ */
+    if (elementoA % 2 === 0) {
+        var resultado = 'par';
+    } else {
+        var resultado = 'ímpar';
+    }
+    // Não altere a linha acima e responda na linha abaixo
+
+    
+    
+    // Não altere a linha abaixo
+    return resultado;
+}
