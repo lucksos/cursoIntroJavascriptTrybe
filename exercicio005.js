ebe48e6a8fed957e4798b02bee4b94a8eb01bb2a
lucksos <luca.trattoria@gmail.com>
2022-02-21 14:31:23 -0300

    Create exercicio005.js

---
+++ exercicio005.js
@@ -0,0 +1,10 @@
+ * Declare e atribua o valor de cada váriável.
+ * Variáveis: meuTamanho, corDaMinhaCamisa, queroEstudarNaTrybe, computadorLigado
+ * e a constante PI
+ */
+
+var meuTamanho = 187;
+const PI = 3.14159;
+var corDaMinhaCamisa = 'naked';
+var queroEstudarNaTrybe = true;
+var computadorLigado = false;
