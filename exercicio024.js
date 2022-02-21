0162bc7af2197c73316e59fa009ab5d15d956e82
lucksos <luca.trattoria@gmail.com>
2022-02-21 14:39:57 -0300

    Create exercicio024.js

---
+++ exercicio024.js
@@ -0,0 +1,18 @@
+function retornaPrimeiroElemento(lista) {
+// não altere a linha acima
+   
+/*
+ * Complete o código atribuindo na variável `resultado`
+ * o primeiro elemento do array `lista` se o array não estiver vazio,
+ * ou a string "array vazio" se o array `lista` estiver vazio.
+ */
+if (lista.length > 0) {
+    var resultado = lista[0];
+} else {
+    var resultado = "array vazio"
+}
+
+// não altere as linhas abaixo
+  return resultado;
+}
+// fim do seu código
