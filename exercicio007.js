90cdab4567454ec3cba6132fd870228778d62e8f
lucksos <luca.trattoria@gmail.com>
2022-02-21 14:32:29 -0300

    Create exercicio007.js

---
+++ exercicio007.js
@@ -0,0 +1,10 @@
+ * Nas linhas abaixo, crie as variáveis `multiplicador` e `multiplicando` e atribua a elas
+ * os valores 4 e 10, respectivamente.
+ *
+ * Em seguida, crie a variável `resultadoDaMultiplicacao` e atribua a ela a multiplicação 
+ * das variáveis criadas acima
+ */
+
+let multiplicador = 4;
+let multiplicando = 10;
+let resultadoDaMultiplicacao = multiplicador * multiplicando;
