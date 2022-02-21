868ba27ceb85f66201248409b27787c64af47d03
lucksos <luca.trattoria@gmail.com>
2022-02-21 14:40:55 -0300

    Create exercicio028.js

---
+++ exercicio028.js
@@ -0,0 +1,20 @@
+function imprimirLista(lista) {
+// não altere a linha acima
+    
+/*
+ * Nas linhas a seguir, escreva um código que imprima usando 
+ * console.log() todos os elementos do array `lista`
+ */
+    for (var c = 0; c <= lista.length - 1;) {
+        console.log(lista[c])
+        c++
+    }
+    
+    
+    
+    
+
+// não altere a linha abaixo
+}
+
+// fim do seu código
