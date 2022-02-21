d173d75e1faedb4cfabd5bec65592488cb335a09
lucksos <luca.trattoria@gmail.com>
2022-02-21 14:43:56 -0300

    Create exercicio034.js

---
+++ exercicio034.js
@@ -0,0 +1,14 @@
+function multiplicaPorDois(numeros) {
+/*
+ * Complete a função multiplicando todos os elementos
+ * do array `numeros` por dois e retornando um array
+ * com esses resultados
+ */
+ var numerosMultiplicados = [];
+ for (var c = 0; c < numeros.length; c++) {
+     numerosMultiplicados.push(numeros[c] * 2);
+ }
+ return numerosMultiplicados;
+}
+
+// fim do seu código
