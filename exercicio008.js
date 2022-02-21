4faaef147c9db1460aa9415af62afe0622484de0
lucksos <luca.trattoria@gmail.com>
2022-02-21 14:33:08 -0300

    Create exercicio008.js

---
+++ exercicio008.js
@@ -0,0 +1,11 @@
+/*
+ * Nas linhas abaixo, crie as variáveis `dividendo` e `divisor` e atribua a elas
+ * os valores 100 e 4, respectivamente.
+ *
+ * Em seguida, crie a variável `resultadoDaDivisao` e atribua a ela a divisão 
+ * da variável dividendo pela variável divisor
+ */
+
+let dividendo = 100;
+let divisor = 4;
+let resultadoDaDivisao = dividendo / divisor;
