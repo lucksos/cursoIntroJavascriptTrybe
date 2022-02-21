9ffd919d0c19c77750bedf69538b219b0b24dd38
lucksos <luca.trattoria@gmail.com>
2022-02-21 14:38:18 -0300

    Create exercicio019.js

---
+++ exercicio019.js
@@ -0,0 +1,16 @@
+function comparacaoEntreElementos(elementoA, elementoB) {
+// não altere a linha acima
+    
+/*
+ * Complete a função atribuindo na 
+ * variável `resultado` a comparação 
+ * `elementoA` OU `elementoB`.
+ */
+  
+  var resultado = elementoA || elementoB;
+
+// não altere as linhas abaixo
+  return resultado;
+}
+
+// fim do seu código
