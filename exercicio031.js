0d0f1a657b5a7a0493ee7e04bc48cb2fbfcddc44
lucksos <luca.trattoria@gmail.com>
2022-02-21 14:42:06 -0300

    Create exercicio031.js

---
+++ exercicio031.js
@@ -0,0 +1,10 @@
+/*
+ * Nas linhas abaixo, escreva uma função chamada `imprimeSaudacao``
+ * que receba um parâmentro chamado `nome` e imprima:
+ * "Olá, ", seguido do valor do parâmetro `nome`, seguido de exclamação (!)
+ */
+ 
+ function imprimeSaudacao(nome) {
+     console.log(`Olá, ${nome}!`)
+ }
+// fim do seu código
