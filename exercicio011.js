a08eae7e2bdd5fc0984e458e306e6c3ffb402767
lucksos <luca.trattoria@gmail.com>
2022-02-21 14:35:04 -0300

    Create exercicio011.js

---
+++ exercicio011.js
@@ -0,0 +1,18 @@
+function exponencial(base, expoente) {
+//não altere a linha acima
+    
+/*
+ * Na linha 36, declare uma variável chamada `potencia`
+ * e atribua a ela o valor da variável `base` elevada a variável
+ * `expoente`
+ */
+  
+    let potencia = base ** expoente;
+    
+
+
+
+//não altere as linhas abaixo
+  return potencia;
+}
+// fim do seu código
