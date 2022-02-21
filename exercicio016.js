3f04a0d93d6f8be05be6c3020c23830cc71c0045
lucksos <luca.trattoria@gmail.com>
2022-02-21 14:36:59 -0300

    Create exercicio016.js

---
+++ exercicio016.js
@@ -0,0 +1,15 @@
+function comparacaoEntreElementos(elementoA, elementoB) {
+// não altere a linha acima
+    
+/*
+ * Atribua como valor da variável `resultado` a verificação:
+ * `elementoA` igual a `elementoB`?
+ */
+  
+  var resultado = elementoA === elementoB;
+
+// não altere as linhas abaixo
+  return resultado;
+}
+
+// fim do seu código
