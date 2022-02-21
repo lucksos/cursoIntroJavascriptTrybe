25f0e1c2433747d6fb1a4ba1b422ea84d0412921
lucksos <luca.trattoria@gmail.com>
2022-02-21 14:35:27 -0300

    Create exercicio012.js

---
+++ exercicio012.js
@@ -0,0 +1,17 @@
+function moduloPorDois(elementoA) {
+// não altere a linha acima
+
+/*
+ * Complete o código deste exercício, atribuindo na 
+ * variável `resultado`, o resto da divisão da variável 
+ * `elementoA` por 2.
+ */
+  
+    
+  var resultado = elementoA % 2;
+
+    
+// não altere as linhas abaixo
+  return resultado;
+}
+// fim do seu código
