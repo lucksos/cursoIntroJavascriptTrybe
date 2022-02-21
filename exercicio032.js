8c1226cad25ee0f0721d3e479bc822a8b63c4499
lucksos <luca.trattoria@gmail.com>
2022-02-21 14:42:35 -0300

    Create exercicio032.js

---
+++ exercicio032.js
@@ -0,0 +1,12 @@
+/*
+ * Crie aqui uma função `somaDoisElementos`, que irá 
+ * receber dois parâmetros, `elementoA` e `elementoB` 
+ * e que retorne a soma desses dois números.
+ */
+function somaDoisElementos(elementoA, elementoB) {
+    var soma = elementoA + elementoB;
+    
+    return soma;
+}
+
+// fim do seu código
